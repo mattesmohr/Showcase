11e79e5cc3415ed523d82a7e788b322bd9feee62
Mattes Mohr <mail@mattesmohr.de>
2025-03-09 19:57:28 +0100

    Remove the global executor

--- Sources/Showcase/Setup.swift
+++ Sources/Showcase/Setup.swift
@@ -15,10 +15,6 @@ enum Setup {
         
         let application = try await Application.make(environment)
         
-        let executorTakeoverSuccess = NIOSingletons.unsafeTryInstallSingletonPosixEventLoopGroupAsConcurrencyGlobalExecutor()
-        
-        application.logger.debug("Running with \(executorTakeoverSuccess ? "SwiftNIO" : "standard") Swift Concurrency default executor")
-        
         application.middleware.use(FileMiddleware(publicDirectory: application.directory.publicDirectory))
         
         do {
